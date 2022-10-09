7c34e5c0799afc920e5e9cb464c74083e3d76153
agussudarmanto <agussudarmanto@users.noreply.github.com>
2022-10-09 22:38:58 +0000

    deploy: 89beca89064e3133ce34ce74b5b38a2ab2c76dbf

--- _nuxt/static/1665354908/projects/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxtjs-tailwind-blog\u002F_nuxt\u002Fstatic\u002F1665354908",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002Fnuxtjs-tailwind-blog\u002F",assetsPath:"\u002Fnuxtjs-tailwind-blog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b3e7d7ea"}}}}(null));
